53e2f5bac4dba08b0c3462a2dc958a07c61f1e05
adnan <sirgeant10@gmail.com>
2024-12-25 12:59:06 +0600

    Add create a service route

--- routes/service.route.js
+++ routes/service.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getServices } from "../controllers/service.controller.js";
+import {
+  createService,
+  getServices,
+} from "../controllers/service.controller.js";
 
 const router = express.Router();
 
 // Get All Services
 router.get("/all-services", getServices);
+
+// Create a service
+router.post("/add-service", createService);
 
 export default router;
