fffe1402cbb124b551d6583c70f87f7cbe087530
adnan <sirgeant10@gmail.com>
2024-12-25 12:33:10 +0600

    Get all services from db

--- routes/service.route.js
+++ routes/service.route.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createService } from "../controllers/service.controller.js";
+import { getServices } from "../controllers/service.controller.js";
 
 const router = express.Router();
 
-// Create a Provider
-router.post("/add-service", createService);
+// Get All Services
+router.get("/all-services", getServices);
 
 export default router;
