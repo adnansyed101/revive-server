740fe97b1d43af4936dad8f3db55556415d922f5
adnan <sirgeant10@gmail.com>
2024-12-25 18:29:52 +0600

    Create update route

--- controllers/service.controller.js
+++ controllers/service.controller.js
@@ -31,6 +31,7 @@ export const createService = async (req, res) => {
   }
 };
 
+// Get All services
 export const getServices = async (req, res) => {
   try {
     const services = await Service.find({});
@@ -41,6 +42,7 @@ export const getServices = async (req, res) => {
   }
 };
 
+// Get single service
 export const getSingleService = async (req, res) => {
   const { id } = req.params;
 
@@ -58,12 +60,35 @@ export const getSingleService = async (req, res) => {
   }
 };
 
+// Get services according to user
 export const getUserCreatedServices = async (req, res) => {
   const { email } = req.params;
 
   try {
     const service = await Service.find({ "provider.email": email });
     res.status(200).json({ success: true, data: service });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+};
+
+// Update a service
+export const updateService = async (req, res) => {
+  const { id } = req.params;
+  const service = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Invalid Service Id" });
+  }
+
+  try {
+    const updatedService = await Service.findByIdAndUpdate(id, service, {
+      new: true,
+    });
+
+    res.status(200).json({ success: true, data: updatedService });
   } catch (err) {
     res.status(500).json({ success: false, message: "Server Error" });
   }
