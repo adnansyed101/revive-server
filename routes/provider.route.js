72166c7861a847607044f4373ed8ce5fb6d05b41
adnan <sirgeant10@gmail.com>
2024-12-25 12:34:28 +0600

    Remove provider
    Keeping provider in a separate db was creating problems in the frontend.
    Was not able to keep the id created by mongo db in the frontend state.
    So, keeping provider information in the service model.

--- routes/provider.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { saveProvider } from "../controllers/provider.controller.js";
-
-const router = express.Router();
-
-// Create a Provider
-router.post("/save-provider", saveProvider);
-
-export default router;
