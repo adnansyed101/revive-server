9cebeea0e25d24e39595d2670cc8708ed85679d7
adnan <sirgeant10@gmail.com>
2024-12-26 21:08:52 +0600

    Add live lik of client side

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: ["http://localhost:5173"],
+  origin: [
+    "http://localhost:5173",
+    "https://reviverewire.web.app",
+    "https://reviverewire.firebaseapp.com/",
+  ],
   credentials: true,
   optionalSuccessStatus: 200,
 };
