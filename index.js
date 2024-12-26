00cc02d18d08e05dc1da38ee0304088f0596a61b
adnan <sirgeant10@gmail.com>
2024-12-26 12:53:00 +0600

    Add cors options

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 connectDB();
 
